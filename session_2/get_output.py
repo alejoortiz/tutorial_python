54c807db52622f7777e59839481d94bf568f589a
alejoortiz <alejandro.ortizvega@gmail.com>
2022-08-09 10:01:15 -0500

    update password request on get_output.py

--- session_2/get_output.py
+++ session_2/get_output.py
@@ -1,11 +1,13 @@
 from netmiko import ConnectHandler
 from getpass import getpass
 
+my_password = getpass()
+
 cisco1 = {
     "device_type": "cisco_xr",
     "host": "10.192.65.121",
     "username": "htts",
-    "password": getpass(),
+    "password": my_password,
 }
 
 with ConnectHandler(**cisco1) as net_connect:
@@ -17,7 +19,7 @@ huawei1 = {
     "device_type": "huawei",
     "host": "10.179.0.64",
     "username": "htts",
-    "password": getpass(),
+    "password": my_password,
 }
 
 with ConnectHandler(**huawei1) as net_connect:
